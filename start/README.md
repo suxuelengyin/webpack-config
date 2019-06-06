dff5f72b3af72f09b6553786955b11c897f814f1
suxue <suxuewb@gmail.com>
2019-06-06 15:56:05 +0800

    change：修改HTML模板配置

---
+++ start/README.md
@@ -0,0 +1,8 @@
+# 插件额外 API
+
+## html-webpack-plugin
+
+该插件为html模板提供了一些列的变量：
+
+### `htmlWebpackPlugin.files`
+
--- start/webpack.config.js
+++ start/webpack.config.js
@@ -7,14 +7,23 @@ module.exports = {
     mode: "production",//构建环境
     output: {
         filename: '[id].[name].[hash:8].js', //添加哈希字符串，添加唯一标识id
-        path: path.resolve(__dirname, 'dist')//必须是绝对路径
+        path: path.resolve(__dirname, 'dist'),//必须是绝对路径
+        publicPath:"", //资源路径的相对url，例如引用cdn的资源
     },
     plugins: [
-        new HtmlWebpackPlugin({        //html 模板加载功能，它自动引入打包后的js
+        new HtmlWebpackPlugin({        //html 模板加载功能，它自动引入打包后的js,css
             title: "基础测试",
-            template:"index.html" //指定模板后，title等属性可能不起作用。
+            filename:'index.html',  //打包后的html文件名
+            template: "index.html", //指定模板后，title等属性可能不起作用。
+            inject: true, //将脚本放在body最后，可以放在head
+            favicon: "", //网站图标的路径，相对路径
+            meta: { //插入meta标签。
+                viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no'
+            }, 
+            base: false, //注入base 标签，为页面上的所有链接规定默认地址或默认目标。
+            minify: false, //生产环境下自动为true
         }),
-        new CleanWebpackPlugin({         // 清理dist文件夹，webpack官方文档已过时。，。。
+        new CleanWebpackPlugin({         // 清理dist文件夹，webpack官方文档已过时。。。
             //模拟文件的删除，默认false
             dry: false,
             //向控制台打印日志，默认false
